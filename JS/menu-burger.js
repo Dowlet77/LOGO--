23231853aaa19e70c236f075803800e4d4d8e486
Dowlet77 <132286379+Dowlet77@users.noreply.github.com>
2024-07-21 23:32:59 +0500

    добавил анимацию меню-бургеру.

--- JS/menu-burger.js
+++ JS/menu-burger.js
@@ -2,8 +2,9 @@ const body = document.body
 const menu = document.querySelector('.menu')
 const menuIcon = document.querySelector('.menu__icon')
 const menuBody = document.querySelector('.menu__body')
-menuIcon.addEventListener('click',function(e){
-    body.classList.toggle('lock')
-    menu.classList.toggle('menu-open')
-    menuBody.classList.toggle('active')
+menuIcon.addEventListener('click', function (e) {
+    menuBody.classList.toggle('_active')
+    menuIcon.classList.toggle('_active')
+    body.classList.toggle('_lock')
 })
+
