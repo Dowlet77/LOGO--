2e1c13732508551dd67e02438a14e659d33d8f36
Dowlet77 <132286379+Dowlet77@users.noreply.github.com>
2024-08-19 19:44:37 +0500

    Доделал меню aside.

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,113 @@
-import { useDynamicAdapt } from './dynamicAdapt.js';
+let menuParents = document.querySelectorAll('.menu-page__parent');
 
-useDynamicAdapt();
+for (let index = 0; index < menuParents.length; index++) {
+    const menuParent = menuParents[index];
+    menuParent.addEventListener('mouseenter', function (e) {
+        menuParent.classList.add('_active');
+    });
+    menuParent.addEventListener('mouseleave', function (e) {
+        menuParent.classList.remove('_active');
+    });
+}
+
+
+
+
+
+const body = document.body
+const menu = document.querySelector('.menu')
+const menuIcon = document.querySelector('.menu__icon')
+const menuBody = document.querySelector('.menu__body')
+menuIcon.addEventListener('click', function (e) {
+    menuIcon.classList.toggle('_active')
+    menuBody.classList.toggle('_active')
+    body.classList.toggle('_lock')
+})
+
+
+
+
+let menuPageBurger = document.querySelector('.menu-page__burger')
+let menuPageBody = document.querySelector('.menu-page__body')
+menuPageBurger.addEventListener('click', function (e) {
+    menuPageBurger.classList.toggle('_active');
+    // menuPageBody.classList.toggle('_active');
+    _slideToggle(menuPageBody);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+let _slideUp = (target, duration = 500) => {
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(() => {
+        target.style.display = 'none';
+        target.style.removeProperty('height');
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+        target.classList.remove('_slide');
+    }, duration);
+}
+
+let _slideDown = (target, duration = 500) => {
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+    if (display === 'none')
+        display = 'block';
+
+    target.style.display = display;
+    let height = target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    window.setTimeout(() => {
+        target.style.removeProperty('height');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+        target.classList.remove('_slide');
+    }, duration);
+}
+let _slideToggle = (target, duration = 500) => {
+    if (!target.classList.contains('_slide')) {
+        target.classList.add('_slide');
+        if (window.getComputedStyle(target).display === 'none') {
+            return _slideDown(target, duration);
+        } else {
+            return _slideUp(target, duration);
+        }
+    }
+}
